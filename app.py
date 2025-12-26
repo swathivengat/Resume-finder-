a473d3457f451a3f8e986553e17ae5571867d68f
swathivengat <swathivengat3101@gmail.com>
2025-12-26 15:00:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -164,5 +164,7 @@ def match_resume():
 def login():
     return render_template('login.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
